3823070379e3e8fb1fefd35a68ffa1ceddf8834c
Rainy Grover <rainygrover57@gamil.com>
2026-02-16 08:55:12 +0530

    Coded uc1 to print OOPS to Console

---
+++ OOPS.java
@@ -0,0 +1,13 @@
+/**
+ * OOPSBannerApp - UC1
+ *
+ * @author rainy
+ * @version 1.0
+ */
+
+public class OOPS {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
